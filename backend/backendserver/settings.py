038812d0f1dffa0089a05d7e2ad0354a2f23808d
yutaokkots <yuta.okkotsu@gmail.com>
2023-05-06 19:04:18 -0600

    updated front end and allowed front end to work on 5173

--- backend/backendserver/settings.py
+++ backend/backendserver/settings.py
@@ -57,8 +57,8 @@ MIDDLEWARE = [
 
 CORS_ALLOWED_ORIGINS = [
     'https://sub.example.com',
-    'http://localhost:3000',
-    'http://127.0.0.1:3000',
+    'http://localhost:5173',
+    'http://127.0.0.1:5173',
 ]
 
 ROOT_URLCONF = 'backendserver.urls'
