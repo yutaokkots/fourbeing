407aef5a41e6484772301b4c2f0a2fba73ecc502
yutaokkots <yuta.okkotsu@gmail.com>
2023-05-11 17:34:03 -0600

    updated models

--- fourbeing/models.py
+++ fourbeing/models.py
@@ -11,6 +11,7 @@ class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
     profile = models.CharField(max_length=300)
     
+    
     def __str__(self):
        return self.user.username
 
