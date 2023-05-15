72e333fa96389e8568d7c058cd4ff000f1dd873e
yutaokkots <yuta.okkotsu@gmail.com>
2023-05-14 18:19:03 -0600

    updated backend openai views

--- translator/views.py
+++ translator/views.py
@@ -11,6 +11,7 @@ API_KEY = os.environ.get('OPENAI_API_KEY')
 #Content-Type: application/json
 # OpenAI key
 language_dict = {
+    "en":"English", 
     "zh":"Mandarin Chinese", 
     "ko":"Korean", 
     "ja":"Japanese", 
@@ -27,13 +28,14 @@ language_dict = {
     "uk":"Ukranian"}
 
 
-
 @api_view(http_method_names=["POST"])
 def translate(request):
     openai.api_key = API_KEY
+   
     prompt = request.data["prompt"]
     lang = request.data["language"]
     language = language_dict[lang]
+    print(request.data)
     try:
         res = openai.Completion.create(
             model="text-davinci-003",
@@ -44,7 +46,11 @@ def translate(request):
             frequency_penalty=0.0,
             presence_penalty=0.0
             )
-        response = res["choices"][0]["text"]
+        response = {
+            "message": "success",
+            "text": res["choices"][0]["text"]
+            }
+        
         return Response(data=response, status=status.HTTP_201_CREATED)
     except Exception as exception:
         return Response(data=exception.args, status=status.HTTP_400_BAD_REQUEST)
